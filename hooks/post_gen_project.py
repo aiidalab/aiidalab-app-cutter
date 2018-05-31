857488ec0798bc0554e0e32b9f06e457f8ab1be4
Leopold Talirz <leopold.talirz@gmail.com>
2018-05-31 18:46:57 +0200

    add note on app registration

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+print("""
+### IMPORTANT###
+Register your app NOW by making a pull request to the Materials Cloud App Registry
+on https://github.com/materialscloud-org/mc-registry
+""")
