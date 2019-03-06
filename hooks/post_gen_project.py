dd3b19e3cca30969412560827cd0adc7759dace6
Casper Welzel Andersen <casper.andersen@epfl.ch>
2019-03-06 13:41:58 +0100

    Version is now part of `metadata.json`.
    Update pre-commit key (`sha` is now `rev`) and hook versions.
    
    Added extra helpful info text in post-cookiecutter hook.
    
    Remove `__version__` from `__init__.py` and only have versioning in `metadata.json`.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,29 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
 print("""
-### IMPORTANT###
-Register your app NOW by making a pull request to the AiiDA Lab App Registry
+Congratulations!
+You have now taken the first step in creating your new AiiDA Lab App:
+
+    {{cookiecutter.app_title}}
+
+### NEXT STEPS ###
+
+1. Open your new AiiDA Lab App folder
+
+    cd {{cookiecutter.repo_name}}
+
+2. Enable version control
+
+    git init
+
+3. Enable code sanity checks on every commit (recommended)
+
+    pip install -U pre-commit
+    pre-commit install
+
+4. Register your app NOW by making a pull request to the AiiDA Lab App Registry
 on https://github.com/aiidalab/aiidalab-registry
+See this link also for a full list of keys for your metadata.json file.
 """)
