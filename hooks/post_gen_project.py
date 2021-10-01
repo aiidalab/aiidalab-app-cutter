5b4b737f1714415d3dd072f9d172920031e84521
Simon Adorf <carl.simon.adorf@gmail.com>
2021-10-01 11:17:30 +0200

    Update post_gen_project hook instructions.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,30 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-
-print("""
+print(
+    """
 Congratulations!
+
 You have now taken the first step in creating your new AiiDAlab App:
 
     {{cookiecutter.app_title}}
 
 ### NEXT STEPS ###
 
-1. Open your new AiiDAlab App folder
+1. Open your new AiiDAlab App folder:
 
     cd {{cookiecutter.repo_name}}
 
-2. Enable version control
+2. Enable version control:
 
     git init
 
-3. Enable code sanity checks on every commit (recommended)
+3. Enable code sanity checks on every commit (recommended):
 
     pip install -U pre-commit
     pre-commit install
 
-4. Register your app NOW by making a pull request to the AiiDAlab App Registry
-on https://github.com/aiidalab/aiidalab-registry
+4. Upload your app repository online, e.g., to GitHub (https://docs.github.com/en/get-started/quickstart/create-a-repo):
 
-See this link also for a full list of recognized metadata keys for the setup.cfg file.
-""")
+    git remote add origin https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.repo_name}}
+    git push -u origin main
+
+5. Register your app NOW by following the instructions here:
+
+   https://aiidalab.readthedocs.io/en/latest/app_development/publish.html
+
+"""
+)
